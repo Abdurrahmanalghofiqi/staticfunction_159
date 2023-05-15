9f06fe65b74810a0b096371ecc0bbd79b048c0b3
Abdurrahmanalghofiqi <abdurrahmanalghofiqi69@gmail.com>
2023-05-15 20:07:39 +0700

    B

--- staticfunction2_159/staticfunction2_159.cpp
+++ staticfunction2_159/staticfunction2_159.cpp
@@ -12,8 +12,8 @@ public:
 	void setID();
 	void printALL();
 
-	static void setNim(int pNim) { nim = pNim;	/*Definisi Function*/
-	static int getNim() { return nim;			/*Definisi Function*/ }
+	static void setNim(int pNim) { nim = pNim;	//*Definisi Function*/
+	static int getNim() { return nim; 	//*Definisi Function*/
 	
 	mahasiswa(string pnama) :nama(pnama) { setID(); }
 };
